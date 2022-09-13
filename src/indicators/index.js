7ecffa9ee19a91bc96dc6207bd5e856cb0ead34b
MisterSzled <greg.baker2@googlemail.com>
2022-09-13 04:07:30 +0100

    Continued work on the fractal

--- src/indicators/index.js
+++ src/indicators/index.js
@@ -1,11 +1,13 @@
 const boomHunter = require("./library/boomHunter");
 const mhull = require("./library/mhull");
 const volatilityOscillator = require("./library/volatilityOscillator");
+const fractal = require("./library/fractal");
 
 let indicators = {
     boomHunter: boomHunter,
     mhull: mhull,
     volatilityOscillator: volatilityOscillator,
+    fractal: fractal,
 }
 
 module.exports = indicators
