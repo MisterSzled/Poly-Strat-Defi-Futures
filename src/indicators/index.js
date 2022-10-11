1e4b01961fc1ec342d8e4fe11c79b9cde13b7cd8
MisterSzled <greg.baker2@googlemail.com>
2022-10-11 12:31:46 +0100

    coral base added in

--- src/indicators/index.js
+++ src/indicators/index.js
@@ -2,12 +2,14 @@ const boomHunter = require("./library/boomHunter");
 const mhull = require("./library/mhull");
 const volatilityOscillator = require("./library/volatilityOscillator");
 const fractal = require("./library/fractal");
+const coralTrend = require("./library/coralTrend");
 
 let indicators = {
     boomHunter: boomHunter,
     mhull: mhull,
     volatilityOscillator: volatilityOscillator,
     fractal: fractal,
+    coralTrend: coralTrend,
 }
 
 module.exports = indicators
---
+++ src/indicators/library/coralTrend.js
@@ -0,0 +1,11 @@
+const cs = require("../../general/chalkSpec");
+
+function getCoral(strat, candleData) {
+    
+}
+
+function coralTrend(strat, candleData) {
+    cs.header("Coral Trend");
+}
+
+module.exports = coralTrend;
