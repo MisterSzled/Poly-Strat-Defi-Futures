84119b79ab7f8ed17e52f04bb42c06bf07d02b02
MisterSzled <greg.baker2@googlemail.com>
2022-10-12 13:27:41 +0100

    coral fix for coutning back backs against bfr

--- src/indicators/library/coralTrend.js
+++ src/indicators/library/coralTrend.js
@@ -1,5 +1,7 @@
 const cs = require("../../general/chalkSpec");
 
+const lookback = 200;
+
 function getCoral(strat, candleData) {
     let priceData = candleData.slice(0, candleData.length - 1);
 
@@ -20,7 +22,7 @@ function getCoral(strat, candleData) {
     let i5 = [];
     let i6 = [];
     let bfr = [];
-    for (let i = priceData.length - 200; i < priceData.length; i++) {
+    for (let i = priceData.length - lookback; i < priceData.length; i++) {
         let src = priceData[i][4];
 
         if (i1.length > 0) {
@@ -59,26 +61,77 @@ function getCoral(strat, candleData) {
     }
 
 
-    console.log("i1: ", i1[i1.length - 1]);
-    console.log("i2: ", i2[i2.length - 1]);
-    console.log("i3: ", i3[i3.length - 1]);
-    console.log("i4: ", i4[i4.length - 1]);
-    console.log("i5: ", i5[i5.length - 1]);
-    console.log("i6: ", i6[i6.length - 1]);
-    console.log("BFR: ", bfr[bfr.length - 1]);
+    // console.log("i1: ", i1[i1.length - 1]);
+    // console.log("i2: ", i2[i2.length - 1]);
+    // console.log("i3: ", i3[i3.length - 1]);
+    // console.log("i4: ", i4[i4.length - 1]);
+    // console.log("i5: ", i5[i5.length - 1]);
+    // console.log("i6: ", i6[i6.length - 1]);
+    // console.log("BFR: ", bfr[bfr.length - 1]);
 
     return bfr;
-    // return bfr[bfr.length - 1] > bfr[bfr.length - 2] ? 1 : 
-    //        bfr[bfr.length - 1] < bfr[bfr.length - 2] ? -1 : 
-    //        0;
+}
+
+function getCleanBarArray(candleData, bfr) {
+    let priceData = candleData.slice(candleData.length - 1 - lookback, candleData.length - 1);
+
+    let bullCountA = -1;
+    let bearCountA = -1;
+
+    let bullCountB = -1;
+    let bearCountB = -1;
+    let cleanBelow = -1;
+    let cleanAbove = -1;
+
+    let count = 0;
+    for (let i = priceData.length - 1; i > 1; i--) {
+        let bfr0 = bfr[bfr.length - 1 - count];
+        let bfr1 = bfr[bfr.length - 2 - count];
+
+        if (bullCountA > -1 && bearCountA > -1 &&
+            bullCountB > -1 && bearCountB > -1 &&
+            cleanBelow > -1 && cleanAbove > -1
+        ) break;
+        if (bullCountA < 0 && (priceData[i][4] > bfr0) && (priceData[i-1][4] < bfr1)) bullCountA = count;
+        if (bearCountA < 0 && (priceData[i][4] < bfr0) && (priceData[i-1][4] > bfr1)) bearCountA = count;
+
+        if (bullCountB < 0 && (priceData[i-1][4] > bfr0) && (priceData[i-2][4] < bfr1)) bullCountB = count;
+        if (bearCountB < 0 && (priceData[i-1][4] < bfr0) && (priceData[i-2][4] > bfr1)) bearCountB = count;
+
+        if (cleanAbove < 0 && (cleanAbove < 0) && (priceData[i][3] > bfr0)) cleanAbove = count;
+        if (cleanBelow < 0 && (cleanBelow < 0) && (priceData[i][2] < bfr0)) cleanBelow = count;
+        count++;
+    }
+
+    return {
+        bull: bullCountB,
+        bear: bearCountB,
+        above: cleanAbove,
+        below: cleanBelow,
+    }
 }
 
 function coralTrend(strat, candleData) {
     cs.header("Coral Trend");
 
-    let coralArray = getCoral(strat, [...candleData]);
+    let bfr = getCoral(strat, [...candleData]);
 
     // Condition 1: is coral bullish
+    let longA = bfr[bfr.length - 1] > bfr[bfr.length - 2];
+    let shortA = bfr[bfr.length - 1] > bfr[bfr.length - 2];
+
+    // XXX
+    // Need to check this works at the threshold of cross over
+    // XXX
+    // Condition 2: At least 1 candle completely above/below (long/short) Coral Trend since last cross above/below (long/short) Coral Trend
+    let cleanBarArray = getCleanBarArray(candleData, bfr);
+    console.log(cleanBarArray)
+
+    let longB = cleanBarArray.above  < cleanBarArray.bull[1];
+    let shortB = cleanBarArray.below < cleanBarArray.bear[1];
+
+    // Condition 3: Pullback closes below/above (long/short) Coral Trend
+
 }
 
 module.exports = coralTrend;
