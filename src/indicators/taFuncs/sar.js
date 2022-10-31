da9301200742fbbc13ce312d75714dffcee93475
MisterSzled <greg.baker2@googlemail.com>
2022-10-31 02:27:43 +0000

    sar indicator

--- src/indicators/taFuncs/sar.js
+++ src/indicators/taFuncs/sar.js
@@ -1,7 +1,15 @@
 // Returns the sma for every possible entry with more than volLength behind it
 
 function sar(candleData, start, inc, max) {
+    let priceData = candleData.slice(0, candleData.length - 2).reverse();
     
+    let result = [];
+    let maxMin = 0;
+    let acceleration = 0;
+
+    for (let i = 0; i < 100; i++) {
+        
+    }
 }
 
 module.exports = sar;
